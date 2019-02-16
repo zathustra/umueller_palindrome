40c6b77c2ff0690d737dca310e9cdcdb0d85e825
Uwe Mueller <mueller.uwe@gmail.com>
2019-02-15 20:56:19 -0500

    Finish working and refactored palindrome method

--- test/umueller_palindrome_test.rb
+++ test/umueller_palindrome_test.rb
@@ -1,11 +1,25 @@
 require "test_helper"
 
 class UmuellerPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::UmuellerPalindrome::VERSION
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+  	assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+  	assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+  	skip
+  end
+
+  def test_palindrome_with_punctuation
+  	assert "Madam. I'm Adam.".palindrome?
+  end
+
 end
