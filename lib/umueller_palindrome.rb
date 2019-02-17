084295ccbd2676ff9c81b719e0f7685d7051a403
Uwe Mueller <mueller.uwe@gmail.com>
2019-02-16 21:11:01 -0500

    Add integer palindrome test

--- lib/umueller_palindrome.rb
+++ lib/umueller_palindrome.rb
@@ -1,6 +1,6 @@
 require "umueller_palindrome/version"
 
-class String
+module UmuellerPalindrome
 
 	#Returns true for a palindrome, false otherwise.
   def palindrome?
@@ -11,6 +11,14 @@ class String
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z0-9]/i).join.downcase
     end
+end
+
+class String
+	include UmuellerPalindrome
+end
+
+class Integer
+	include UmuellerPalindrome
 end
